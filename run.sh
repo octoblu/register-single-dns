f7b57ac8ed21bbaa1405bc9eeb87b203f631b81d
sqrtofsaturn <sqrt@octoblu.com>
2015-11-24 18:38:42 -0700

    initial version

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "${IP}" ]; then
+  echo "Missing IP, exiting."
+  exit 1
+fi
+
+if [ -z "${RECORD}" ]; then
+  echo "Missing RECORD, exiting."
+  exit 1
+fi
+
+if [ -z "${DOMAIN}" ]; then
+  echo "Missing DOMAIN, exiting."
+  exit 1
+fi
+
+HOSTED_ZONE=$(aws route53 list-hosted-zones-by-name --dns-name ${DOMAIN} | jq -r ".HostedZones[] | select(.Name==\"${DOMAIN}.\") .Id")
+
+if [ -z "${HOSTED_ZONE}" ]; then
+  echo "Unable to find zone for ${DOMAIN}, exiting."
+  exit 1
+fi
+
+cat change.template | sed -e "s/%IP%/${IP}/g" | sed -e "s/%RECORD%/${RECORD}/g" > change.json
+aws route53 change-resource-record-sets --hosted-zone ${HOSTED_ZONE} --change-batch file://change.json
